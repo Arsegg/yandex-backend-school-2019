5dd133704b342c9d0548b591c6787101691bf317
Arsegg <arsegg@mail.ru>
2019-07-30 11:44:00 +0300

    added mongodb integration

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 
+from ext import db
 from imports import imports
 
 
 def create_app():
-    app_ = Flask(__name__)
-    app_.register_blueprint(imports, url_prefix="/imports")
-    return app_
+    app = Flask(__name__)
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb://db/imports",
+        "connect": False,
+    }
+    db.init_app(app)
 
+    app.register_blueprint(imports, url_prefix="/imports")
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    return app

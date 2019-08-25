49b0636d9dd681bc725ed67233ab7706c7ffdbf3
Arsegg <arsegg@mail.ru>
2019-08-26 00:22:03 +0300

    added some usefull stuff

--- models.py
+++ models.py
@@ -1,5 +1,15 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+from marshmallow import post_load
+from numpy import percentile
+
 from ext import (db,
                  ma, )
+
+relationship = db.Table("relationship",
+                        db.Column("citizen_id", db.ForeignKey("citizen.id"), primary_key=True),
+                        db.Column("relative_id", db.ForeignKey("citizen.id"), primary_key=True))
 
 
 class Citizen(db.Model):
@@ -11,18 +21,70 @@ class Citizen(db.Model):
     building = db.Column(db.String)
     apartment = db.Column(db.Integer)
     name = db.Column(db.String)
-    birth_date = db.Column(db.String)
+    birth_date = db.Column(db.Date)
     gender = db.Column(db.String)
-    relative_id = db.Column(db.Integer, db.ForeignKey("citizen.id"))
-    relatives = db.relationship("Citizen")
+    # Many-To-Many relationship
+    relatives = db.relationship("Citizen", secondary=relationship, primaryjoin=(id == relationship.c.citizen_id),
+                                secondaryjoin=(id == relationship.c.relative_id), lazy="dynamic")
+
+    def get_age(self):
+        return relativedelta(datetime.now(), self.birth_date).years
+
+    def ensure_relationship(self):
+        self.relatives = [getattr(self, "import").get_citizen(relative.citizen_id)
+                          for relative in self.relatives]
+        for relative in self.relatives:
+            if self not in relative.relatives:
+                relative.relatives.append(self)
+
+    def remove_relationship(self):
+        for relative in self.relatives:
+            if self in relative.relatives:
+                relative.relatives.remove(self)
 
     def __repr__(self):
-        return f"Citizen({self.citizen_id})"
+        return f"Citizen({self.citizen_id}, import_id={self.import_id}, id={self.id})"
 
 
 class Import(db.Model):
     import_id = db.Column(db.Integer, primary_key=True)
-    citizens = db.relationship("Citizen")
+    citizens = db.relationship("Citizen", backref="import", lazy="dynamic")
+
+    @classmethod
+    def get(cls, import_id):
+        return cls.query.get(import_id)
+
+    def get_citizen(self, citizen_id):
+        return self.citizens.filter_by(citizen_id=citizen_id).one()
+
+    def get_birthdays(self):
+        birthdays = {month + 1: [] for month in range(12)}
+
+        for citizen in self.citizens:
+            for month in birthdays:
+                presents = citizen.relatives.filter(db.extract("month", Citizen.birth_date) == month).count()
+                if presents == 0:
+                    continue
+                entry = dict(citizen_id=citizen.citizen_id,
+                             presents=presents)
+                birthdays[month].append(entry)
+
+        return birthdays
+
+    def get_stats(self):
+        stats = []
+
+        for citizen in self.citizens.distinct(Citizen.town):
+            town = citizen.town
+            birthdays = [citizen.get_age() for citizen in self.citizens.filter_by(town=town)]
+            p50, p75, p99 = percentile(birthdays, (50, 75, 99,))
+            entry = dict(town=town,
+                         p50=p50,
+                         p75=p75,
+                         p99=p99)
+            stats.append(entry)
+
+        return stats
 
     def __repr__(self):
         return f"Import({self.import_id})"
@@ -31,14 +93,36 @@ class Import(db.Model):
 class CitizenSchema(ma.ModelSchema):
     relatives = ma.Pluck("self", "citizen_id", many=True)
 
+    # @post_load(pass_many=True)
+    # def post_load(self, data, many, **kwargs):
+    #     if not many:
+    #         data["relatives"] = [self.context["import"].get_citizen(relative.citizen_id) for relative in
+    #                              data["relatives"]]
+    #
+    #     return data
+
     class Meta:
+        dateformat = "%d.%m.%Y"
         exclude = ("id",)
+        # include_fk = True
         model = Citizen
         sqla_session = db.session
 
 
 class ImportSchema(ma.ModelSchema):
     citizens = ma.Nested(CitizenSchema, many=True)
+
+    @post_load
+    def post_load(self, data, **kwargs):
+        for citizen in data.citizens:
+            self.session.add(citizen)
+
+        citizens = {citizen.citizen_id: citizen for citizen in data.citizens}
+        for citizen in data.citizens:
+            citizen.relatives = [citizens[relative.citizen_id] for relative in citizen.relatives]
+            self.session.add(citizen)
+
+        return data
 
     class Meta:
         model = Import
